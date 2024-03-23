333e5caf235efbeb2e8d12bf2282e2ada5053204
oscar <oscar@LAPTOP-3QHK9JSN>
2024-03-23 13:25:25 +0100

    essai 1

--- src/main/java/fr/insa/mathieu/architecture_officielle/Architecture_officielle.java
+++ src/main/java/fr/insa/mathieu/architecture_officielle/Architecture_officielle.java
@@ -1,7 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
+// verification du code 
 package fr.insa.mathieu.architecture_officielle;
 
 /*
