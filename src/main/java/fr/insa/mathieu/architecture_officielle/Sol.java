dafddd25c4cfd4bb84ec406463162ad13ba2de0b
oscar <oscar@LAPTOP-3QHK9JSN>
2024-03-26 09:43:16 +0100

    test

--- src/main/java/fr/insa/mathieu/architecture_officielle/Sol.java
+++ src/main/java/fr/insa/mathieu/architecture_officielle/Sol.java
@@ -3,16 +3,77 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package fr.insa.mathieu.architecture_officielle;
-
+import static fr.insa.mathieu.architecture_officielle.Mur.longueur;
+import static java.lang.Math.sqrt;
 /**
  *
  * @author stard
  */
 public class Sol {
     private String id;
-    private double surface_sol;
+    private Coin supg;// coin supérieur gauche
+    private Coin supd;// coin supérieur droit
+    private Coin infg;// coin inférieur gauche
     private double prix_sol;
     private Revêtement revêtement_sol;
+    
+    public Sol(Coin supg, Coin supd, Coin infg){
+        this.id ="idc";
+        this.supg = supg;
+        this.supd = supd;
+        this.infg = infg;
+    }
+
+    
+    
+  public static double surface (Coin supg, Coin supd, Coin infg){
+      double surface = longueur(supg,supd)*longueur (supg,infg);
+      return surface; 
+  }
+     public Sol(Revêtement revêtement) {
+        this.revêtement_sol = revêtement;
+        // Il faudrait ajouter le prix, mais pour ça il faut la surface du mur
+    }
+    
+     public static double prix (Sol sol,Revêtement rev){
+        double prix= surface (sol.getSupg(),sol.getSupd() ,sol.getInfg())* rev.getPrix_unitaire();
+        return prix;
+     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Coin getSupg() {
+        return supg;
+    }
+
+    public void setSupg(Coin supg) {
+        this.supg = supg;
+    }
+
+    public Coin getSupd() {
+        return supd;
+    }
+
+    public void setSupd(Coin supd) {
+        this.supd = supd;
+    }
+
+    public Coin getInfg() {
+        return infg;
+    }
+
+    public void setInfg(Coin infg) {
+        this.infg = infg;
+    }
+
+
+
     
     
     
