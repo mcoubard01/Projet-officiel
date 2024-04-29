a2e020fb3032d87420d08f1dd452dad027ca6eb2
oscar <oscar@LAPTOP-3QHK9JSN>
2024-04-29 18:58:12 +0200

    ATTENTION  pour le moment tout ne fonctionne pas

--- src/main/java/fr/insa/mathieu/architecture_officielle/Pièce.java
+++ src/main/java/fr/insa/mathieu/architecture_officielle/Pièce.java
@@ -91,17 +91,23 @@ public static String indente (String toIndente, String prefix){
 
     
     // en dessous ce sont des test pas forcement utile mais permmette de vérifiez si ca fonctionne. 
-public static Pièce piècetest(){
+public static void  piècetest(){
     Etage e1 = new Etage(2);
     Coin c1 =new Coin("c1",1,2);
     Coin c2 =new Coin("c2",1,6);
     Coin c3 =new Coin("c3",5,6);
     Coin c4 =new Coin("c4",5,2);
+    Coin c5 =new Coin("c5",7,2);
+    Coin c6 =new Coin("c6",7,6);
     
     Mur m1= new Mur("m1",c1,c2,e1);
     Mur m2= new Mur("m2",c2,c3,e1);  
     Mur m3= new Mur("m3",c3,c4,e1); 
-    Mur m4= new Mur("m4",c4,c1,e1); 
+    Mur m4= new Mur("m4",c4,c1,e1);
+     Mur m5= new Mur("m5",c4,c5,e1);
+     Mur m6= new Mur("m6",c5,c6,e1);
+      Mur m7= new Mur("m7",c6,c3,e1);
+      
     Pièce pièce1= new Pièce("p1");
    
      pièce1.add(c1);
@@ -112,20 +118,33 @@ public static Pièce piècetest(){
     pièce1.add(m2);
     pièce1.add(m3);
     pièce1.add(m4);
-    return pièce1;
+    System.out.println(pièce1);
+    
+    Pièce pièce2= new Pièce("p2");
+   
+    pièce2.add(c3);
+    pièce2.add(c4);
+    pièce2.add(c5);
+    pièce2.add(c6);
+    pièce2.add(m3);
+    pièce2.add(m5);
+    pièce2.add(m6);
+    pièce2.add(m7);
+    
+       System.out.println(pièce2);
+
+    
 
 }
 
 public static void test1(){
    
-    System.out.println ("est c e que ca marche");
-Pièce.piècetest();
-
-        System.out.println("pièce test : \n"+ Pièce.piècetest());   
+    
+   
     System.out.println("la surface est du sol est de : "+sol.trouverEtCalculerSurface() );   // ceci n'est pas nécessaire 
-   System.out.println( "le prix est du sol  :"+ sol.trouverEtCalculerPrix()+"€");
+   System.out.println( "le prix est du sol  :"+ sol.trouverEtCalculerPrix()+" €");
    System.out.println("la surface est du plafond est de : "+plafond.trouverEtCalculerSurface() );   // ceci n'est pas nécessaire
-   System.out.println( "le prix est du plafond  :"+ plafond.trouverEtCalculerPrix()+"€");
+   System.out.println( "le prix est du plafond  :"+ plafond.trouverEtCalculerPrix()+" €");
    
     System.out.println();
 }
@@ -133,7 +152,9 @@ Pièce.piècetest();
 
 
 public static void main (String[] args){
-    test1();
+  System.out.println ("est c e que ca marche"); 
+  piècetest();
+  System.out.println("la surface est du sol est de : "+sol.trouverEtCalculerSurface() ); 
 }
 
 }
