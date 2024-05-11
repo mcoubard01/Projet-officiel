f44aed7ee7742f854a53a44ea4d5c11a706bb8e1
oscar <oscar@LAPTOP-3QHK9JSN>
2024-05-11 11:00:38 +0200

    modification de la classe façade ce que j'ai fait fonctionne mais il faut vérifier pour les revêtement car ca ne marche pas a part si je prend le 9999

--- src/main/java/fr/insa/mathieu/architecture_officielle/Facade.java
+++ src/main/java/fr/insa/mathieu/architecture_officielle/Facade.java
@@ -22,15 +22,28 @@ public class Facade extends Mur  {
     private ArrayList<Ouverture> liste_ouverture;
     private ArrayList<Etage> liste_étage;
 
-    public Facade(Coin debut, Coin fin) {
-         this.id = IDManager.newId(this);
+    public Facade(Coin debut, Coin fin,Revêtement revêtement_facade) {
+          super(debut,fin, revêtement_facade);
+          this.id = IDManager.newId(this);
          this.debut = debut;
         this.fin = fin;
-       
+      this.revêtement_facade = revêtement_facade;
         this.liste_ouverture = new ArrayList<>();
         this.liste_étage = new ArrayList<>();
 
     }
+    /*
+       public Facade(Coin debut, Coin fin) {
+          super(debut,fin);
+          this.id = IDManager.newId(this);
+         this.debut = debut;
+        this.fin = fin;
+      Revêtement standard = new Revêtement(9999);
+        this.liste_ouverture = new ArrayList<>();
+        this.liste_étage = new ArrayList<>();
+
+    }
+    */
 //TODO faire le prix pour cette classe
     
     
@@ -41,26 +54,30 @@ public class Facade extends Mur  {
         for (i = 0; i < liste_étage.size(); i++) {
             hauteur_bat = hauteur_bat + liste_étage.get(i).getHauteur_etage();
         }
+       
 double surface_ouverture=0;
-System.out.println("la taille de la liste est de"+ liste_ouverture.size() );
 
 int j;
-for (j = 0; j < liste_ouverture.size(); j++) {
-    // TODO rajoutez une vérification d'appartenance a la facade mais je ne sait pas commment rédiger ça 
-    if (liste_ouverture.get(j).getLongueur()==0.90){
-        surface_ouverture= surface_ouverture + (liste_ouverture.get(j).getLongueur()*2.10);
+
+
+System.out.println("la taille de la liste d'ouverture est de"+ this.getListe_ouverture().size() );
+ 
+for (j = 0; j < this.getListe_ouverture().size(); j++) {
+    
+        if (this.getListe_ouverture().get(j).getLongueur()==0.90){
+        surface_ouverture= surface_ouverture + (this.getListe_ouverture().get(j).getLongueur()*2.10);
  System.out.println("c'est une porte");
     }else{
-         surface_ouverture= surface_ouverture + (liste_ouverture.get(j).getLongueur()*1.20);
+         surface_ouverture= surface_ouverture + (this.getListe_ouverture().get(j).getLongueur()*1.20);
         System.out.println("c'est une fenetre");
-    }
+    } 
  
 }
         double s = (longueur(debut,fin)*hauteur_bat)-surface_ouverture ;
-
         return s;
-
     }
+    
+  
 
     public static void main(String[] args) {
 
@@ -81,23 +98,24 @@ for (j = 0; j < liste_ouverture.size(); j++) {
         // créer et initialiser l'ArrayList
       
         Etage Etage1 = new Etage(2);
-        
+       Revêtement revet_facade= new Revêtement (9999);
          Etage Etage2 = new Etage(2);
         Etage Etage3 = new Etage(2);
         
         Coin c1 = new Coin(1, 1);
         Coin c2 = new Coin(1, 5);
-        Mur m1= new Mur(c1,c2,Etage2);
-        Fenêtre Fenêtre1= new Fenêtre(1,1,'E',m1);
-        Porte Porte1= new Porte(1,1,'E',m1);
+          Facade F1 = new Facade(c1, c2, revet_facade);
+          
+        Fenêtre Fenêtre1= new Fenêtre(1,2,'N',F1);
+        Porte Porte1= new Porte(3,1,'E',F1);
          
-        Facade F1 = new Facade(c1, c2);
-         F1.liste_étage.add(Etage1);
+      
+        F1.liste_étage.add(Etage1);
         F1.liste_étage.add(Etage2);
         F1.liste_étage.add(Etage3);
-        F1.liste_ouverture.add(Fenêtre1);
-              F1.liste_ouverture.add(Porte1);
+    
         System.out.println("la surface de la facade est de " +F1.surface() );
+        System.out.println("le prix de la facade est de " +F1.prix() );
 
 
     }
